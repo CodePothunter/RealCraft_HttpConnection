82d63e8d5125d8330e3c56626efffdcf560c75d5
CodePothunter <wuxueyang94@gmail.com>
2015-06-05 15:56:44 +0800

    Update Json analyser

--- app/src/main/java/com/example/user/realcraft/UpdateInfo.java
+++ app/src/main/java/com/example/user/realcraft/UpdateInfo.java
@@ -1,10 +1,13 @@
 package com.example.user.realcraft;
 
 import android.util.Log;
+import android.util.Pair;
 import android.widget.Toast;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -98,6 +101,95 @@ public class UpdateInfo {
             default:;
                 String Progress = "采集了成功";
                 Log.d("SHOW",Progress);
+        }
+    }
+
+    private List<String> convertStringToJSONFirstTime(String source){
+        List<String> pairs = new ArrayList<String>();
+        String playerInfo = "empty";
+        String resourceInfo = "empty";
+        String constructorInfo = "empty";
+        int i = 0;
+        String[] sourceArray = source.split("/");
+        try {
+            playerInfo = "[" + sourceArray[0] + "]";
+        } catch(Exception e){
+            Log.d("Test", "用户信息获取失败");
+        }
+        try{
+            resourceInfo  = "["+sourceArray[1].substring(1);
+            for(i = 2; i < sourceArray.length ;++i){
+                if(!sourceArray[i].substring(0,1).equals("1"))break;
+                resourceInfo = ","+sourceArray[i].substring(1);
+            }
+            resourceInfo = resourceInfo + "]";
+        } catch (Exception e){
+            Log.d("Test","获取资源失败");
+        }
+        try{
+            constructorInfo = "[" + sourceArray[i].substring(1);
+            for(;i<sourceArray.length;++i){
+                constructorInfo = ","+sourceArray[i].substring(1);
+            }
+            constructorInfo = constructorInfo+"]";
+        } catch (Exception e){
+            Log.d("Test","获取建造物失败");
+        }
+        pairs.add(playerInfo);
+        pairs.add(resourceInfo);
+        pairs.add(constructorInfo);
+        return pairs;
+    }
+
+    public void analyseAllPointsFirstTime(List<String> sourceArray){
+
+        String playerInfo = sourceArray.get(0);
+        String resourceInfo = sourceArray.get(1);
+        String constructorInfo = sourceArray.get(2);
+        String id = null, location = null;
+        String playId = null, playWood = null, playStone = null, playFood = null, playWorkForce = null;
+        List<Pair<String,String>> resourceArray = new ArrayList<>();
+        List<Pair<String,String>> constructorArray = new ArrayList<>();
+        //获取用户信息
+        try{
+            JSONArray jsonArray = new JSONArray(playerInfo);
+            for(int i = 0; i < jsonArray.length(); ++i){
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                playId = jsonObject.getString("playerId");
+                playWood = jsonObject.getString("wood");
+                playStone = jsonObject.getString("stone");
+                playFood = jsonObject.getString("food");
+                playWorkForce = jsonObject.getString("workforce");
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        //获取资源，仅可获得资源的位置和id
+        try{
+            JSONArray jsonArray = new JSONArray(resourceInfo);
+            for(int i = 0; i < jsonArray.length(); ++i) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                id = jsonObject.getString("id");
+                location = jsonObject.getString("location");
+                Pair<String,String> tmp = new Pair<String,String>(id,location);
+                resourceArray.add(tmp);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        //获取建造物，仅可获得资源的位置和id
+        try{
+            JSONArray jsonArray = new JSONArray(resourceInfo);
+            for(int i = 0; i < jsonArray.length(); ++i) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                id = jsonObject.getString("id");
+                location = jsonObject.getString("location");
+                Pair<String,String> tmp = new Pair<String,String>(id,location);
+                constructorArray.add(tmp);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
